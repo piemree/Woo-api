bf4039bc6717301b8438ff8f1c166d31a027f5b1
Emre Demir <pi.emree@gmail.com>
2021-08-19 12:42:41 +0300

    Update gitignore

--- dist/config/default.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    port: 5000,
-    host: "localhost",
-    dbUri: "mongodb://localhost:27017",
-};
